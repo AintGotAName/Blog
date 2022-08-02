232add3e4f5a36830d57b16cce83fd28caf381ae
unknown <xuantho573@gmail.com>
2022-08-02 11:32:09 +0700

    Export functions from authentication.js

--- backend/config/index.js
+++ backend/config/index.js
@@ -1,4 +1,17 @@
 import { connectDatabase } from "./database.js";
 import generateKeyPair from "./keyPair.js";
+import {
+    validatePassword,
+    generatePassword,
+    authenticationMiddleware,
+    issueJWT,
+} from "./authentication.js";
 
-export { connectDatabase, generateKeyPair };
+export {
+    connectDatabase,
+    generateKeyPair,
+    validatePassword,
+    generatePassword,
+    authenticationMiddleware,
+    issueJWT,
+};
