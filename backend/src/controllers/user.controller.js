cfd125ae0c0efbdb476c95b8ac0f68db12100b31
unknown <xuantho573@gmail.com>
2022-08-02 12:36:00 +0700

    Initialize register middleware

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -40,6 +40,47 @@ class UserController {
             console.log(`Error detected while authenticating user!\n`);
         }
     }
+
+    // register
+    // [POST]
+    async register(req, res) {
+        console.log("Registering new user!\n");
+        try {
+            const username = req.body.username;
+            const user = await User.findOne({ username: username });
+            if (user)
+                res.status(409).json({
+                    success: false,
+                    msg: `There is a user with the given username, please try another username!`,
+                });
+            else {
+                const password = generatePassword(req.body.password);
+                const newUser = new User({
+                    username: username,
+                    hash: password.hash,
+                    salt: password.salt,
+                    information: {},
+                    blogsList: [],
+                    followers: [],
+                    following: [],
+                    saved: [],
+                });
+                newUser.save();
+                const jwt = issueJWT(newUser);
+                res.status(201).json({
+                    success: true,
+                    msg: `New user created successfully!`,
+                    token: jwt,
+                });
+            }
+        } catch (err) {
+            console.log(`Error detected while creating new user!\n`);
+            res.status(401).json({
+                success: false,
+                msg: `Something happend while registering new user!`,
+            });
+        }
+    }
 }
 
 export default UserController;
