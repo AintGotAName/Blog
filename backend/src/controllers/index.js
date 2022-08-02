b37328a34b12534f465665f270e6b0c45897597c
unknown <xuantho573@gmail.com>
2022-08-02 07:15:25 +0700

    Export controllers

---
+++ backend/src/controllers/index.js
@@ -0,0 +1,4 @@
+import UserController from "./user.controller.js";
+import BlogController from "./blog.controller.js";
+
+export { UserController, BlogController };
--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -1,3 +1,5 @@
 import { User } from "../models/index.js";
 
 class UserController {}
+
+export default UserController;
