e4e72d0f8e471f719797e3f3b55bce54a7fca6b6
unknown <xuantho573@gmail.com>
2022-08-04 10:27:36 +0700

    Improve logging and add home-route for testing

--- backend/index.js
+++ backend/index.js
@@ -1,15 +1,25 @@
 import express from "express";
 import * as dotenv from "dotenv";
 import mongoose from "mongoose";
+import cors from "cors";
 
-import { connectDatabase } from "./config/index.js";
+import { connectDatabase, useMiddlewares } from "./config/index.js";
 
 const app = express();
+dotenv.config();
+
 const port = process.env.PORT || 8000;
 
-dotenv.config();
 connectDatabase(mongoose, process.env.DATABASE_CONNECTION);
+useMiddlewares(app, [
+    cors(),
+    express.json(),
+    express.urlencoded({ extended: false }),
+]);
+app.get("/", (req, res) => {
+    res.json({ status: "haiz" });
+});
 
 app.listen(port, () => {
-    console.log(`App listening or port ${port}`);
+    console.log(`App listening or port ${port}\n`);
 });
