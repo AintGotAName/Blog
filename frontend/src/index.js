bf1b57a6893f8510cf33617935c7153aae28341c
unknown <xuantho573@gmail.com>
2022-08-04 21:47:53 +0700

    Initialize client

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,10 +1,17 @@
 import { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+import { CookiesProvider } from "react-cookie";
+
+import App from "./App.js";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
     <StrictMode>
-        <BrowserRouter></BrowserRouter>
+        <CookiesProvider>
+            <BrowserRouter>
+                <App />
+            </BrowserRouter>
+        </CookiesProvider>
     </StrictMode>
 );
