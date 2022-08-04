1777288d6513fb0c3ddeeaf15a6d06f8650dfc4c
unknown <xuantho573@gmail.com>
2022-08-04 21:00:03 +0700

    Remove unnecessary lines

---
+++ frontend/src/index.js
@@ -0,0 +1,10 @@
+import { StrictMode } from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+    <StrictMode>
+        <BrowserRouter></BrowserRouter>
+    </StrictMode>
+);
