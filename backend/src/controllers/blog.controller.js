78f6510b14c99ce1df2117166783f6f77782fb11
unknown <xuantho573@gmail.com>
2022-08-04 10:34:41 +0700

    Change controller from class to functions

--- backend/src/controllers/blog.controller.js
+++ backend/src/controllers/blog.controller.js
@@ -1,33 +1,31 @@
 import { Blog } from "../models/index.js";
 
-class BlogController {
-    // get a specific blog
-    // [GET]
-    async getBlog(req, res) {
-        try {
-            const blog = await Blog.findById(req.params._id);
-            if (blog) {
-                res.status(200).json({
-                    success: true,
-                    msg: `Get the blog successfully!`,
-                    data: blog,
-                });
-                console.log(`Get the blog successfully!\n`);
-            } else {
-                res.status(404).json({
-                    success: false,
-                    msg: `Cannot find the blog, maybe it hasn't been created yet!`,
-                });
-                console.log(`There is no blog with the given _id!\n`);
-            }
-        } catch (err) {
-            console.log(`Error detected while getting the blog!\n`);
+// get a specific blog
+// [GET]
+const getBlog = async (req, res) => {
+    try {
+        const blog = await Blog.findById(req.params._id);
+        if (blog) {
+            res.status(200).json({
+                success: true,
+                msg: `Get the blog successfully!`,
+                data: blog,
+            });
+            console.log(`Get the blog successfully!\n`);
+        } else {
             res.status(404).json({
                 success: false,
-                msg: `Something happened while getting the blog, please try again!`,
+                msg: `Cannot find the blog, maybe it hasn't been created yet!`,
             });
+            console.log(`There is no blog with the given _id!\n`);
         }
+    } catch (err) {
+        console.log(`Error detected while getting the blog!\n`);
+        res.status(404).json({
+            success: false,
+            msg: `Something happened while getting the blog, please try again!`,
+        });
     }
-}
+};
 
-export default BlogController;
+export { getBlog };
