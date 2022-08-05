a983e7d10ad2233d55af9ae0ce9f73e918204b37
unknown <xuantho573@gmail.com>
2022-08-05 14:22:41 +0700

    Initialize skeleton for user information

--- frontend/src/pages/user/components/Information.js
+++ frontend/src/pages/user/components/Information.js
@@ -1,2 +1,20 @@
-const Information = () => {};
+const Information = ({ username, user, joinedAt }) => {
+    const joined = (joinedAt ? new Date(joinedAt) : new Date())
+        .toUTCString()
+        .split(" ");
+    return (
+        <>
+            <h1>{username}</h1>
+            {user.fields.map((field, index) => (
+                <h2 key={index}>
+                    {index ? `, ` : ``}
+                    {field}
+                </h2>
+            ))}
+            <h3>Joined: {`${joined[2]} ${joined[1]}, ${joined[3]}`}</h3>
+            <h3>Location: {user.location}</h3>
+            <h3>Email: {user.email}</h3>
+        </>
+    );
+};
 export default Information;
--- frontend/src/pages/user/index.js
+++ frontend/src/pages/user/index.js
@@ -1,6 +1,6 @@
 import { useNavigate } from "react-router-dom";
 import { useCookies } from "react-cookie";
-import { useLayoutEffect, useState } from "react";
+import { useEffect, useState } from "react";
 
 import BlogsList from "./components/BlogsList.js";
 import Followers from "./components/Followers.js";
@@ -8,16 +8,21 @@ import Following from "./components/Following.js";
 import Information from "./components/Information.js";
 
 const User = ({ url }) => {
-    const [user, setUser] = useState({});
+    const [user, setUser] = useState({
+        information: {
+            fields: [],
+            location: "",
+            email: "",
+        },
+    });
     const [cookies] = useCookies(["token"]);
     const navigate = useNavigate();
 
-    useLayoutEffect(() => {
+    useEffect(() => {
         if (cookies.token) {
             fetch(url, { headers: { authorization: cookies.token } })
                 .then((res) => res.json())
                 .then((res) => {
-                    console.log(res.user);
                     setUser(res.user);
                 });
         } else {
@@ -26,8 +31,11 @@ const User = ({ url }) => {
     }, []);
     return (
         <>
-            <h1>{user.username}</h1>
-            <Information />
+            <Information
+                username={user.username}
+                user={user.information}
+                joinedAt={user.joinedAt}
+            />
             <BlogsList />
             <Followers />
             <Following />
