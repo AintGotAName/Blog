cb0600a8a6869fea4e0ef93597f498e8f2603847
unknown <xuantho573@gmail.com>
2022-08-12 14:48:15 +0700

    Add route for Login and Home pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,2 +1,18 @@
-const App = () => {};
+import { Routes, Route } from "react-router-dom";
+import Login from "./pages/login";
+import User from "./pages/user";
+const App = () => {
+    return (
+        <Routes>
+            <Route
+                path="/"
+                element={<User url="http://localhost:5000/user" />}
+            />
+            <Route
+                path="/login"
+                element={<Login url="http://localhost:5000/user/login" />}
+            />
+        </Routes>
+    );
+};
 export default App;
