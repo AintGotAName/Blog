961a9afb03296c0c7939baa67a979da1e9c996ee
unknown <xuantho573@gmail.com>
2022-08-14 14:31:22 +0700

    Fix bug to display 'fields' in 1 line

--- frontend/src/pages/user/components/Information.js
+++ frontend/src/pages/user/components/Information.js
@@ -5,12 +5,14 @@ const Information = ({ username, user, joinedAt }) => {
     return (
         <>
             <h1>{username}</h1>
-            {user.fields.map((field, index) => (
-                <h2 key={index}>
-                    {index ? `, ` : ``}
-                    {field}
-                </h2>
-            ))}
+            <h2>
+                {user.fields.map(
+                    (field, index) => `
+                    ${index ? `, ` : ``}
+                    ${field}
+                    `
+                )}
+            </h2>
             <h3>Joined: {`${joined[2]} ${joined[1]}, ${joined[3]}`}</h3>
             <h3>Location: {user.location}</h3>
             <h3>Email: {user.email}</h3>
