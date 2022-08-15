601359325a5785280bbca7bc90dad1b326f456d1
unknown <xuantho573@gmail.com>
2022-08-15 15:07:33 +0700

    Add styling and initialize section for BlogsList

--- frontend/src/pages/user/index.js
+++ frontend/src/pages/user/index.js
@@ -7,14 +7,13 @@ import Followers from "./components/Followers.js";
 import Following from "./components/Following.js";
 import Information from "./components/Information.js";
 
+import styles from "./styling/index.module.css";
+
 const User = ({ url }) => {
     const [user, setUser] = useState({
-        information: {
-            fields: [],
-            location: "",
-            email: "",
-        },
+        information: { name: "", fields: [], location: "", email: "" },
     });
+    const [blogs, setBlogs] = useState([]);
     const [cookies] = useCookies(["token"]);
     const navigate = useNavigate();
 
@@ -23,23 +22,25 @@ const User = ({ url }) => {
             fetch(url, { headers: { authorization: cookies.token } })
                 .then((res) => res.json())
                 .then((res) => {
+                    console.log(res.user);
                     setUser(res.user);
+                    setBlogs(res.user.blogsList);
                 });
         } else {
             navigate("/login", { replace: true });
         }
     }, []);
     return (
-        <>
+        <div className={`${styles.page} flex--down center`}>
             <Information
                 username={user.username}
                 user={user.information}
                 joinedAt={user.joinedAt}
             />
-            <BlogsList />
+            <BlogsList blogs={blogs} />
             <Followers />
             <Following />
-        </>
+        </div>
     );
 };
 
