f4f1e9217f4fe50da52b11bbcf7086b4474cf607
unknown <xuantho573@gmail.com>
2022-08-15 15:03:32 +0700

    Add route for Blog

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import { Routes, Route } from "react-router-dom";
 import Login from "./pages/login";
 import User from "./pages/user";
+import Blog from "./pages/blog";
 import "./App.css";
 
 const App = () => {
@@ -14,6 +15,7 @@ const App = () => {
                 path="/login"
                 element={<Login url="http://localhost:5000/user/login" />}
             />
+            <Route path="/blog/:id" element={<Blog />} />
         </Routes>
     );
 };
