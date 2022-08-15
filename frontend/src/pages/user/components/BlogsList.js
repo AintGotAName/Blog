39616765d73db887e1d895a79584e71c3e0655e8
unknown <xuantho573@gmail.com>
2022-08-15 15:05:52 +0700

    Initialize BlogsList

--- frontend/src/pages/user/components/BlogsList.js
+++ frontend/src/pages/user/components/BlogsList.js
@@ -1,2 +1,24 @@
-const BlogsList = () => {};
+import { Link, useNavigate } from "react-router-dom";
+
+import styles from "../styling/BlogsList.module.css";
+
+const BlogsList = ({ blogs }) => {
+    const navigate = useNavigate();
+    const handler = async (id) => {
+        navigate(`/blog/${id}`);
+    };
+    return (
+        <div className={`${styles.container} flex--down center`}>
+            {blogs.map((blog) => (
+                <div
+                    style={{ width: "100%" }}
+                    onClick={() => handler(blog._id)}
+                    key={blog._id}
+                >
+                    {blog.name}
+                </div>
+            ))}
+        </div>
+    );
+};
 export default BlogsList;
