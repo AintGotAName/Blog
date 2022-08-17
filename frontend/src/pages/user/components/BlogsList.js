2d0205356409e2f7cecfff65c092b58a04cc226c
unknown <xuantho573@gmail.com>
2022-08-17 14:23:33 +0700

    Fetch blogs to show 'saved' and 'liked'

--- frontend/src/pages/user/components/BlogsList.js
+++ frontend/src/pages/user/components/BlogsList.js
@@ -1,26 +1,59 @@
+import { useEffect, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
+
+import ThumbUpOutlinedIcon from "@mui/icons-material/ThumbUpOutlined";
+import SaveAltOutlinedIcon from "@mui/icons-material/SaveAltOutlined";
 
 import styles from "../styling/BlogsList.module.css";
 
 const BlogsList = ({ blogs }) => {
+    const [newBlogs, setNewBlogs] = useState([]);
     const navigate = useNavigate();
     const handler = async (id) => {
         navigate(`/blog/${id}`);
     };
+
+    useEffect(() => {
+        blogs.forEach(async (blog) => {
+            const res = await fetch(`http://localhost:5000/blog/${blog._id}`);
+            const data = (await res.json()).data;
+            setNewBlogs((prev) =>
+                prev.concat({
+                    id: data._id,
+                    name: data.name,
+                    saved: data.saved,
+                    liked: data.liked,
+                    preview: data.content.slice(0, 50),
+                })
+            );
+        });
+    }, [blogs]);
+
     return (
-        <div
-            className={`${styles.container} flex--down center mg16 pd16 border`}
-        >
-            {blogs.map((blog) => (
+        <>
+            {newBlogs.map((blog) => (
                 <div
-                    className={`${styles.blog} pd8 border`}
-                    onClick={() => handler(blog._id)}
-                    key={blog._id}
+                    className={`${styles.blog} pd16 border mg8 flex--down`}
+                    onClick={() => handler(blog.id)}
+                    key={blog.id}
                 >
-                    {blog.name}
+                    <Link to={`/blog/${blog.id}`}>{blog.name}</Link>
+
+                    <p>{blog.preview}...</p>
+                    <div className={`flex--right`}>
+                        <div>
+                            <ThumbUpOutlinedIcon></ThumbUpOutlinedIcon>
+
+                            {blog.liked}
+                        </div>
+                        <div>
+                            <SaveAltOutlinedIcon></SaveAltOutlinedIcon>
+                            {blog.saved}
+                        </div>
+                    </div>
                 </div>
             ))}
-        </div>
+        </>
     );
 };
 export default BlogsList;
